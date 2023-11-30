a4d1ca33ffe103925175a7ecdfa3300aacf48caf
dottg0n20 <dev@dariocaruso.info>
2023-11-30 17:43:55 +0100

    better counter

--- libs/queries.ts
+++ libs/queries.ts
@@ -94,7 +94,7 @@ export async function getPublicStories(opts?: {
       (story as unknown as any).image =
         `/stories/${story._id}/panels/${panels[0]._id}/image`;
     }
-    (story as unknown as any).panelsCount = panels.length + 1;
+    (story as unknown as any).panelsCount = panels.length;
   }
   return stories;
 }
@@ -111,11 +111,14 @@ export async function getMyStories(
     .limit(opts?.limit || 10)
     .lean();
   for (const story of stories) {
-    const panel = await db.panels.findOne({ story: story._id }).lean();
-    if (panel) {
+    const panels = await db.panels
+      .find({ story: story._id }, { _id: true })
+      .lean();
+    if (panels[0]) {
       (story as unknown as any).image =
-        `/stories/${story._id}/panels/${panel._id}/image`;
+        `/stories/${story._id}/panels/${panels[0]._id}/image`;
     }
+    (story as unknown as any).panelsCount = panels.length;
   }
   return stories;
 }
